26994620ed5f7e5428c8aa4f694ad92e329976bb
SolFreeman <soloviyov.stas@gmail.com>
2025-01-22 17:07:14 +0200

    bugfix

--- build/assets/js/global.js
+++ build/assets/js/global.js
@@ -9,30 +9,30 @@ jQuery(document).ready(function ($) {
 	})
 
 	function handleTabClicks() {
-        if ($(window).width() > 767) {
-            $('ul.tabs__caption').on('click', 'li:not(.active)', function () {
-                $(this)
-                    .addClass('active').siblings().removeClass('active')
-                    .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
-            });
-        } else {
-            $('ul.tabs__caption').off('click', 'li:not(.active)');
-        }
-    }
+		if ($(window).width() > 767) {
+			$('ul.tabs__caption').on('click', 'li:not(.active)', function () {
+				$(this)
+					.addClass('active').siblings().removeClass('active')
+					.closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+			});
+		} else {
+			$('ul.tabs__caption').off('click', 'li:not(.active)');
+		}
+	}
 
-    // Initial check
-    handleTabClicks();
+	// Initial check
+	handleTabClicks();
 
-    // Check on window resize
-    $(window).resize(function() {
-        handleTabClicks();
-    });
+	// Check on window resize
+	$(window).resize(function () {
+		handleTabClicks();
+	});
 
 	function selectLang() {
 		var nav = $('.language-title');
 		var selection = $('.language-list');
 		var select = selection.find('li');
-	
+
 		nav.click(function (event) {
 			if (nav.hasClass('active')) {
 				nav.removeClass('active');
@@ -44,18 +44,18 @@ jQuery(document).ready(function ($) {
 			event.preventDefault();
 			event.stopPropagation(); // Останавливаем всплытие события, чтобы не сработал обработчик для документа
 		});
-	
+
 		select.click(function (event) {
 			select.removeClass('active');
 			$(this).addClass('active');
 			var $lang = $(this).text();
 			nav.text($lang);
 			nav.trigger('click');
-	
+
 			//event.preventDefault();
 			//event.stopPropagation(); // Останавливаем всплытие события, чтобы не сработал обработчик для документа
 		});
-	
+
 		$(document).click(function () {
 			if (nav.hasClass('active')) {
 				nav.removeClass('active');
@@ -65,24 +65,24 @@ jQuery(document).ready(function ($) {
 	}
 
 	selectLang();
-	
+
 	function handleResize() {
-        if ($(window).width() < 768) {
-            $('.title').off('click').on('click', function() {
-                $(this).toggleClass('active');
-                $(this).next('.info-list').slideToggle();
-            });
-        } else {
-            $('.title').off('click'); // Убираем обработчик клика на десктопе
-            $('.info-list').removeAttr('style'); // Убираем инлайн-стили, установленные slideToggle
-        }
-    }
+		if ($(window).width() < 768) {
+			$('.title').off('click').on('click', function () {
+				$(this).toggleClass('active');
+				$(this).next('.info-list').slideToggle();
+			});
+		} else {
+			$('.title').off('click'); // Убираем обработчик клика на десктопе
+			$('.info-list').removeAttr('style'); // Убираем инлайн-стили, установленные slideToggle
+		}
+	}
 
-    handleResize(); // Первоначальная проверка при загрузке страницы
+	handleResize(); // Первоначальная проверка при загрузке страницы
 
-    $(window).resize(function() {
-        handleResize(); // Проверка при изменении размера окна
-    });
+	$(window).resize(function () {
+		handleResize(); // Проверка при изменении размера окна
+	});
 
 	$('.slider-news').slick({
 		slidesToShow: 3,
@@ -104,7 +104,7 @@ jQuery(document).ready(function ($) {
 			},
 		]
 	});
-	
+
 	/**
 	document.getElementById('copyButton').addEventListener('click', function() {
 		var textField = document.getElementById('textField');
@@ -115,42 +115,44 @@ jQuery(document).ready(function ($) {
 		});
 	});
 	**/
-	
-	$('.pricing-option label').click(function() {       
-        var this_price = $(this).data('price');		
+
+	$('.pricing-option label').click(function () {
+		var this_price = $(this).data('price');
 		$('.final-price span').text(this_price);
-    });
-	
-	$('.tablist a').click(function(event) {
-        event.preventDefault(); 
-        $('.tablist a').removeClass('active'); 
-        $(this).addClass('active');
-    });
+	});
 
-	$('.next').click(function() {
-        $('.step_1').hide();
-        $('.step_2, .info-text').show();
-    });
+	$('.tablist a').click(function (event) {
+		event.preventDefault();
+		$('.tablist a').removeClass('active');
+		$(this).addClass('active');
+	});
 
-    $('.back').click(function() {
-        $('.step_2, .info-text').hide();
-        $('.step_1').show();
-    });
+	$('.next').click(function () {
+		$('.step_1').hide();
+		$('.step_2, .info-text').show();
+	});
+
+	$('.back').click(function () {
+		$('.step_2, .info-text').hide();
+		$('.step_1').show();
+	});
 
 	openAccountNav();
 
+	addActiveModalItem();
+
 	document.querySelectorAll('.toggle-password').forEach(button => {
 		button.addEventListener('click', () => {
-		  const input = button.previousElementSibling; // находим соседний input
-		  
-		  // Переключаем тип поля
-		  if (input.type === 'password') {
-			input.type = 'text';
-			button.classList.add('visible'); // Добавляем класс
-		  } else {
-			input.type = 'password';
-			button.classList.remove('visible'); // Убираем класс
-		  }
+			const input = button.previousElementSibling; // находим соседний input
+
+			// Переключаем тип поля
+			if (input.type === 'password') {
+				input.type = 'text';
+				button.classList.add('visible'); // Добавляем класс
+			} else {
+				input.type = 'password';
+				button.classList.remove('visible'); // Убираем класс
+			}
 		});
 	});
 
@@ -184,7 +186,7 @@ function faqAccordeon() {
 }
 
 function openAccountNav() {
-	var nav = $('.btn-box');
+	var nav = $('.registered-user .btn-box');
 	var selection = $('.account-list');
 
 	nav.click(function (event) {
@@ -206,3 +208,16 @@ function openAccountNav() {
 		}
 	});
 }
+
+function addActiveModalItem() {
+	$('.open_edit_info_item').on('click', function () {
+		$('.edit-list .item').removeClass('active');
+		$('.edit_info_item').addClass('active');
+	});
+
+	$('.open_edit_pass_item').on('click', function () {
+		$('.edit-list .item').removeClass('active');
+		$('.edit_pass_item').addClass('active');
+	});
+}
+
